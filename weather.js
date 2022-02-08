5519186d6c10d83005ea8e4637f013ee1a61db07
Shirl Firefly <sfirefly@student.21-school.ru>
2022-02-08 21:20:47 +0700

    Вывод в консоль

--- weather.js
+++ weather.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
+import {printHelp} from "./services/log.service.js";
 
 const initCLI = () => {
     const args = getArgs(process.argv);
-    console.log(args);
+    // console.log(args);
     if (args.h) {
         // Вывод help
+        printHelp();
     }
     if (args.s) {
         // Сохранить город
